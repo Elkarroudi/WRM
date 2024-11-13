e86231a74c44cacf0e0febf7c233157536dd9b65
Elkarroudi <amineelkarroudi04@proton.me>
2024-11-13 12:16:13 +0100

    Create Waiting list Entity

--- src/main/java/org/wora/wrm/entities/WaitingList.java
+++ src/main/java/org/wora/wrm/entities/WaitingList.java
@@ -3,48 +3,45 @@ package org.wora.wrm.entities;
 import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.wora.wrm.entities.enums.AlgorithmType;
+import org.wora.wrm.entities.enums.WorkMode;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "visitors")
-public class Visitor implements Serializable {
+@Table(name = "waitingLists")
+public class WaitingList implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
 
     @Column(
-            name = "firstName",
-            nullable = false
-    )
-    private String firstName;
-
-    @Column(
-            name = "lastName",
-            nullable = false
-    )
-    private String lastName;
-
-    @Column(
-            name = "email",
+            name = "date",
             unique = true,
             nullable = false
     )
-    private String email;
+    private LocalDate date;
 
     @Column(
-            name = "phone",
+            name = "algorithmType",
             nullable = false
     )
-    private String phone;
+    private AlgorithmType algorithmType;
 
     @Column(
-            name = "age",
+            name = "workMode",
             nullable = false
     )
-    private short age;
+    private WorkMode workMode;
+
+    @Column(
+            name = "capacity",
+            nullable = false
+    )
+    private int capacity;
 
 
     @CreationTimestamp
@@ -56,5 +53,6 @@ public class Visitor implements Serializable {
 
     @UpdateTimestamp
     private LocalDateTime updatedAt;
+
 
 }
