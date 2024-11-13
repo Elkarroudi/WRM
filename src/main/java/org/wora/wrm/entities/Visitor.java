79cb1681d5a39b5114c287712ff4b54772470cab
Elkarroudi <amineelkarroudi04@proton.me>
2024-11-13 11:52:31 +0100

    Create Visitor Entity

---
+++ src/main/java/org/wora/wrm/entities/Visitor.java
@@ -0,0 +1,59 @@
+package org.wora.wrm.entities;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "visitors")
+public class Visitor implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(
+            name = "firstName",
+            nullable = false
+    )
+    private String firstName;
+
+    @Column(
+            name = "lastName",
+            nullable = false
+    )
+    private String lastName;
+
+    @Column(
+            name = "email",
+            unique = true,
+            nullable = false
+    )
+    private String email;
+
+    @Column(
+            name = "phone",
+            nullable = false
+    )
+    private String phone;
+
+    @Column(
+            name = "age",
+            nullable = false
+    )
+    private short age;
+
+    @CreationTimestamp
+    @Column(
+            updatable = false,
+            nullable = false
+    )
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+}
