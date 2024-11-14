0590adb16d3d72f1361d94fc498c74457c443bf3
Elkarroudi <amineelkarroudi04@proton.me>
2024-11-14 09:56:25 +0100

    Mdf DELETE method return type

--- src/main/java/org/wora/wrm/services/IGenericService.java
+++ src/main/java/org/wora/wrm/services/IGenericService.java
@@ -1,7 +1,6 @@
 package org.wora.wrm.services;
 
 import org.springframework.data.domain.Page;
-import org.wora.wrm.utils.apiResponse.ApiResponse;
 
 public interface IGenericService<CreateDTO, UpdateDTO, ResponseDTO> {
 
@@ -9,6 +8,6 @@ public interface IGenericService<CreateDTO, UpdateDTO, ResponseDTO> {
     ResponseDTO findById(String id);
     ResponseDTO save(CreateDTO dto);
     ResponseDTO update(String id, UpdateDTO dto);
-    ApiResponse delete(String id);
+    void delete(String id);
 
 }
